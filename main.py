03ed3bdc126d5b39582b143972cd04a25a3d32f1
Krakburg <kevinroncolato@outlook.com>
2024-10-12 21:10:36 +0100

    Timeout Check, RST and FIN Flag check

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ capture = pyshark.LiveCapture(interface='wlan0')
 
 
 current_sessions = {}
+bwd_count = 0
+fwd_count = 0
+del_session_count = 0
+timeout = 100
 
 for packet in capture:
     try:
@@ -11,36 +15,60 @@ for packet in capture:
             timestamp = packet.sniff_time.timestamp()
             src_ip = packet.ip.src
             dst_ip = packet.ip.dst
-            
             proto_num = packet.ip.proto
-            
-            if "TCP" in packet:
-                src_port = packet.tcp.srcport
-                dst_port = packet.tcp.dstport
-            
-            if "UDP" in packet:
-                src_port = packet.udp.srcport
-                dst_port = packet.udp.dstport
+                
+            src_port = packet[packet.transport_layer].srcport
+            dst_port = packet[packet.transport_layer].dstport
                 
             fwd_session = src_ip + dst_ip + src_port + dst_port
             bwd_session = dst_ip + src_ip + dst_port + src_port
             
+            # Check if fwd Session
             if fwd_session in current_sessions.keys():
-                pass
+                fwd_count +=1
+                
+                # Check for Reset or Fin flag
+                if "TCP" in packet and (packet.tcp.flags_fin.int_value == 1 or packet.tcp.flags_reset.int_value == 1):
+                    current_sessions[fwd_session].new_packet(packet)
+                    del(current_sessions[fwd_session])
+                    del_session_count += 1
+                
+                # Update packet
+                else:
+                    current_sessions[fwd_session].new_packet(packet)
+                    
+                    
+            # Check if bwd Session
             elif bwd_session in current_sessions.keys():
-                pass
+                bwd_count += 1
+                
+                # Check for Reset or Fin flag
+                if "TCP" in packet and (packet.tcp.flags_fin.int_value == 1 or packet.tcp.flags_reset.int_value == 1):
+                    current_sessions[bwd_session].new_packet(packet)
+                    del(current_sessions[bwd_session])
+                    del_session_count += 1
+                    
+                # Update packet
+                else:
+                    current_sessions[bwd_session].new_packet(packet);
+
+            # Make new Session      
             else:
                 session = Session(packet)
                 current_sessions[fwd_session] = session
             
-#         if "IPV6" in packet:
-#             src_ip = packet.ipv6.src
-#             dst_ip = packet.ipv6.dst
-#             proto_num = packet.ipv6.nxt
+            # Stats
+            print(str(len(current_sessions)),'\t\t', str(fwd_count), '\t\t', str(bwd_count), '\t\t', str(del_session_count))
+            for session_id, session_class in current_sessions.items():
+                print(session_class.get_src_ip(),'\t\t', timestamp - session_class.get_packet_timestamp(), '\t\t', session_class.get_packet_timestamp(), session_class.get_syn_count())
+            print('\n')
         
-        for selected_session in current_sessions.keys():
-            #print(current_sessions[selected_session].get_src_ip())
-            print(str(len(current_sessions)))
+        
+        #Check all sessions for timeouts
+        for session_id, session_class in current_sessions.copy().items():
+            if timestamp - session_class.get_packet_timestamp() >= timeout:
+                del(current_sessions[session_id])
+                del_session_count += 1
 
-    except AttributeError as error:
-        pass
+    except Exception as error:
+        print(error)
--- session.py
+++ session.py
@@ -1,5 +1,10 @@
 class Session:
     def __init__(self, packet):
+        
+        self.timestamp = packet.sniff_time.timestamp()
+        self.packet_timestamp = self.timestamp
+        self.duration = self.timestamp - self.packet_timestamp
+        
         self.src_ip = packet.ip.src
         self.dst_ip = packet.ip.dst
         
@@ -16,17 +21,50 @@ class Session:
         self.fwd_session = self.src_ip + self.dst_ip + self.src_port + self.dst_port
         self.bwd_session = self.dst_ip + self.src_ip + self.dst_port + self.src_port
         
-        self.timestamp = 0
+        
         self.fin_count = 0
         self.syn_count = 0
         self.rst_count = 0
         self.psh_count = 0
         self.ack_count = 0
         self.urg_count = 0
-        self.cwe_count = 0
+        self.cwr_count = 0
         self.ece_count = 0
+        
         
         
     def get_src_ip(self):
         return self.src_ip
+    
+    def get_fin_count(self):
+        return self.fin_count
+    
+    def get_rst_count(self):
+        return self.rst_count
+    
+    def get_syn_count(self):
+        return self.syn_count
+    
+    def get_duration(self):
+        return self.duration
+        
+    def get_packet_timestamp(self):
+        return self.packet_timestamp
+    
+    def new_packet(self, packet):
+        self.packet_timestamp = packet.sniff_time.timestamp()
+        self.duration = self.packet_timestamp - self.timestamp
+        
+        if "TCP" in packet:
+            self.fin_count += packet.tcp.flags_fin.int_value
+            self.syn_count += packet.tcp.flags_syn.int_value
+            self.rst_count += packet.tcp.flags_reset.int_value
+            self.psh_count += packet.tcp.flags_push.int_value
+            self.ack_count += packet.tcp.flags_ack.int_value
+            self.urg_count += packet.tcp.flags_urg.int_value
+            self.cwr_count += packet.tcp.flags_cwr.int_value
+            self.ece_count += packet.tcp.flags_ece.int_value
+            
+        
+        
 
