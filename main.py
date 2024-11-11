e989ac3d09311b6e9d59181f4d00b695c6f7b90d
Kevin Stephens <kevinroncolato@outlook.com>
2024-11-11 02:23:03 +0000

    Cleaner, faster and some issues fixed

--- main.py
+++ main.py
@@ -1,31 +1,110 @@
 import pyshark
-
-def print_callback(packet):
-    print(packet)
-
+import logging
+from session import FlowSession
 capture = pyshark.LiveCapture(interface='wlan0')
 
+# CREATE LOG FILE, REVIEW RST FLAG, GET MORE DATA COLLECTED(ALL PREFERABLY)
+
+logging.basicConfig(
+    filename="ids.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+current_flows = {}
+del_flow_count = 0
+timeout = 70
+timestamp = 0
+
+
+def handle_packet(packet):
+    global del_flow_count
+    try:
+        if "IP" not in packet:
+            return
+
+        src_ip = packet.ip.src
+        dst_ip = packet.ip.dst
+        proto_num = packet.ip.proto
+        src_port = packet[packet.transport_layer].srcport
+        dst_port = packet[packet.transport_layer].dstport
+
+        # Define forward and backward flows
+        fwd_flow_id = src_ip + "-" + dst_ip + "-" + src_port + "-" + dst_port + "-" + proto_num
+        bwd_flow_id = dst_ip + "-" + src_ip + "-" + dst_port + "-" + src_port + "-" + proto_num
+
+        # Select flow direction and check if it exists
+        flow_direction, flow_key = ("FWD", fwd_flow_id) if fwd_flow_id in current_flows else ("BWD", bwd_flow_id)
+        
+        if flow_key in current_flows:
+            # Handle Reset or Fin flag if packet is TCP
+            if packet.transport_layer == "TCP":
+                
+                if packet.tcp.flags_fin.int_value == 1 or packet.tcp.flags_reset.int_value == 1:
+                                        
+                    current_flows[flow_key].new_packet(packet, flow_direction)
+                    
+                    current_flows[flow_key].get_final_data()
+                    
+                    logging.info(f"DURATION {current_flows[flow_key].get_final_data()}")
+                    
+                    del current_flows[flow_key]
+                    
+                    del_flow_count += 1
+                    
+                    logging.info(f"Deleted flow {flow_key}, FIN/RST flag")
+                
+                else:
+                    current_flows[flow_key].new_packet(packet, flow_direction)
+        else:
+            # Create a new flow if neither fwd nor bwd flows are found
+            current_flows[fwd_flow_id] = FlowSession(packet, "FWD")
+            current_flows[fwd_flow_id].new_packet(packet, "FWD")
+            logging.info(f"Created flow {flow_key}")
+            
+    except Exception as error:
+        print(error)
+
+
+def list_flows():
+    global del_flow_count
+    print(f"Current Flows: {str(len(current_flows))}  Deleted Flows: {str(del_flow_count)}")
+    
+    print(f"{'Src IP':<20} {'Dst IP':<20} {'Last Seen':<15}")
+    
+    for flow_id, flow in current_flows.items():
+        print(f"{str(flow.get_src_ip()):<20} {str(flow.get_dst_ip()):<20} {str(timestamp - flow.get_packet_last_seen()):<20}")
+    print('\n')
+
+
+def check_flow_timeout():
+    global del_flow_count
+    
+    # Check all flows for timeouts
+    for flow_key, flow in current_flows.copy().items():
+        if (timestamp - flow.get_packet_last_seen()) >= timeout:
+            
+            current_flows[flow_key].get_final_data()
+            
+            logging.info(f"DURATION {current_flows[flow_key].get_final_data()}")
+            
+            print(current_flows[flow_key].get_final_data())
+            
+            del(current_flows[flow_key])
+            del_flow_count += 1
+            
+            logging.info(f"Deleted flow {flow_key}, TIMEOUT")
+            
+            
 
 for packet in capture:
-    try:
-        src_ip = None
-        dst_ip = None
-        proto_num = None
-        
-        if "IP" in packet:
-            src_ip = packet.ip.src
-            dst_ip = packet.ip.dst
-            proto_num = packet.ip.proto
-            
-            
-        if "IPV6" in packet:
-            src_ip = packet.ipv6.src
-            dst_ip = packet.ipv6.dst
-            proto_num = packet.ipv6.nxt
-        
-        timestamp = float(packet.sniff_time.timestamp())
-        if src_ip != None and dst_ip != None:
-            print(src_ip, dst_ip, proto_num, timestamp)
+    
+    timestamp = packet.sniff_time.timestamp()
+    
+    handle_packet(packet)
+    
+    list_flows()
 
-    except AttributeError as error:
-        pass
+    check_flow_timeout()
+    
+
---
+++ session.py
@@ -0,0 +1,241 @@
+import numpy as np
+
+class FlowSession:
+    def __init__(self, packet, direction):
+        
+        self.src_ip = packet.ip.src
+        self.dst_ip = packet.ip.dst
+        
+        if "TCP" in packet:
+            self.src_port = packet.tcp.srcport
+            self.dst_port = packet.tcp.dstport
+            
+        if "UDP" in packet:
+            self.src_port = packet.udp.srcport
+            self.dst_port = packet.udp.dstport
+            
+        self.fwd_session = self.src_ip + self.dst_ip + self.src_port + self.dst_port
+        self.bwd_session = self.dst_ip + self.src_ip + self.dst_port + self.src_port
+        
+        self.proto = packet.ip.proto
+        
+        self.timestamp = packet.sniff_time.timestamp()
+        self.packet_last_seen = self.timestamp
+        self.get_flow_last_seen = 0
+        self.flow_duration = 0
+        
+        self.total_bytes = 0
+        
+        self.tot_fwd_pkts = 0
+        self.tot_bwd_pkts = 0
+        
+        self.totlen_fwd_pkts = 0
+        self.totlen_bwd_pkts = 0
+        
+        self.fwd_pkt_len_max = 0
+        self.fwd_pkt_len_min = 0
+        self.fwd_pkt_len_mean = 0
+        self.fwd_pkt_len_std = 0
+        
+        self.fwd_pkt_len_list = []
+        self.bwd_pkt_len_list = []
+        
+        self.bwd_pkt_len_max = 0
+        self.bwd_pkt_len_min = 0
+        self.bwd_pkt_len_mean = 0
+        self.bwd_pkt_len_std = 0
+        
+        self.flow_byts_s = 0
+        self.flow_pkts_s = 0
+        
+        self.flow_iat_mean = 0
+        self.flow_iat_std = 0
+        self.flow_iat_max = 0
+        self.flow_iat_min = 0
+        
+        self.fwd_iat_tot = 0
+        self.fwd_iat_mean = 0
+        self.fwd_iat_std = 0
+        self.fwd_iat_max = 0
+        self.fwd_iat_min = 0
+        
+        self.bwd_iat_tot = 0
+        self.bwd_iat_mean = 0
+        self.bwd_iat_std = 0
+        self.bwd_iat_max = 0
+        self.bwd_iat_min = 0
+        
+        self.fwd_psh_flags = 0
+        self.bwd_psh_flags = 0
+        
+        self.fwd_urg_flags = 0
+        self.bwd_urg_flags = 0
+        
+        self.fwd_header_len = 0
+        self.bwd_header_len = 0
+        
+        self.fwd_pkts_s = 0
+        self.bwd_pkts_s = 0
+        
+        self.pkt_len_min = 0
+        self.pkt_len_max = 0
+        self.pkt_len_mean = 0
+        self.pkt_len_std = 0
+        self.pkt_len_var = 0
+        
+        self.fin_flag_cnt = 0
+        self.syn_flag_cnt = 0
+        self.rst_flag_cnt = 0
+        self.psh_flag_cnt = 0
+        self.ack_flag_cnt = 0
+        self.urg_flag_cnt = 0
+        self.cwe_flag_cnt = 0
+        self.ece_flag_cnt = 0
+        
+        self.down_up_ratio = 0
+        
+        self.pkt_size_avg = 0
+        
+        self.fwd_seg_size_avg = 0
+        self.bwd_seg_size_avg = 0
+        
+        self.fwd_byts_b_avg = 0
+        self.fwd_pkts_b_avg = 0
+        self.fwd_blk_rate_avg = 0
+        
+        self.bwd_byts_b_avg = 0
+        self.bwd_pkts_b_avg = 0
+        self.bwd_blk_rate_avg = 0
+        
+        self.subflow_fwd_pkts = 0
+        self.subflow_fwd_byts = 0
+        self.subflow_bwd_pkts = 0
+        self.subflow_bwd_byts = 0
+        
+        self.init_fwd_win_byts = 0
+        self.init_bwd_win_byts = 0
+        
+        self.fwd_act_data_pkts = 0
+        self.fwd_seg_size_min = 0
+        
+        self.active_mean = 0
+        self.active_std = 0
+        self.active_max = 0
+        self.active_min = 0
+        
+        self.idle_mean = 0
+        self.idle_std = 0
+        self.idle_max = 0
+        self.idle_min = 0
+        
+           
+    def get_src_ip(self):
+        return self.src_ip
+    
+    def get_dst_ip(self):
+        return self.dst_ip
+    
+    def get_packet_last_seen(self):
+        return self.packet_last_seen
+    
+    def set_packet_last_seen(self, packet):
+        self.packet_last_seen = packet.sniff_time.timestamp()
+    
+    def new_packet(self, packet, direction):
+        
+        self.set_packet_last_seen(packet)
+        
+        self.total_bytes += len(packet)
+        
+        # Check if packet is fwd direction
+        if direction  == "FWD":
+            
+            # Increase fwd total
+            self.tot_fwd_pkts += 1;
+            
+            # Add to total fwd packet lenght
+            self.totlen_fwd_pkts = self.totlen_fwd_pkts + int(packet.length)
+            
+            # Check if the packet is the largest yet
+            if (self.fwd_pkt_len_max < int(packet.length)):
+                self.fwd_pkt_len_max = int(packet.length)
+            
+            # Check if the packet is the smallest yet
+            if (self.fwd_pkt_len_min > int(packet.length)):
+                self.fwd_pkt_len_min = int(packet.length)
+            
+            # Append packet lenght to the forward packet length list
+            self.fwd_pkt_len_list.append(int(packet.length))
+            
+            
+        # Check if packet is bwd direction
+        if direction  == "BWD":
+            
+            # Increase bwd total
+            self.tot_bwd_pkts += 1;
+            
+            # Add to total bwd packet lenght
+            self.totlen_bwd_pkts = self.totlen_bwd_pkts + int(packet.length)
+            
+            # Check if the packet is the largest yet
+            if (self.bwd_pkt_len_max < int(packet.length)):
+                self.bwd_pkt_len_max = int(packet.length)
+            
+            # Check if the packet is the smallest yet
+            if (self.bwd_pkt_len_min > int(packet.length)):
+                self.bwd_pkt_len_min = int(packet.length)
+            
+            # Append packet lenght to the backward packet lenght
+            self.bwd_pkt_len_list.append(int(packet.length))
+                
+            
+        # Add Set flags to count
+        if "TCP" in packet:
+            self.fin_flag_cnt += packet.tcp.flags_fin.int_value
+            self.syn_flag_cnt += packet.tcp.flags_syn.int_value
+            self.rst_flag_cnt += packet.tcp.flags_reset.int_value
+            self.psh_flag_cnt += packet.tcp.flags_push.int_value
+            self.ack_flag_cnt += packet.tcp.flags_ack.int_value
+            self.urg_flag_cnt += packet.tcp.flags_urg.int_value
+            self.cwe_flag_cnt += packet.tcp.flags_cwr.int_value
+            self.ece_flag_cnt += packet.tcp.flags_ece.int_value
+    
+    
+    def get_final_data(self):
+        
+        self.flow_duration = self.get_packet_last_seen() - self.timestamp
+        
+        # Bytes/s
+        if self.total_bytes != 0 and self.flow_duration != 0:
+            self.flow_byts_s = self.total_bytes / self.flow_duration
+        else:
+            self.flow_byts_s = 0
+        
+        # Packet len mean
+        if self.flow_duration > 0:
+            self.fwd_pkt_len_mean = self.totlen_fwd_pkts / self.tot_fwd_pkts
+        else:
+            self.fwd_pkt_len_mean = 0
+            
+        if self.tot_bwd_pkts != 0:
+            self.bwd_pkt_len_mean = self.totlen_bwd_pkts / self.tot_bwd_pkts
+        else:
+            self.bwd_pkt_len_mean = 0
+        
+        # Packet len std
+        if self.fwd_pkt_len_list:
+            self.fwd_pkt_len_std = np.std(self.fwd_pkt_len_list)
+        else:
+            self.fwd_pkt_len_std = 0
+            
+        if self.bwd_pkt_len_list:
+            self.bwd_pkt_len_std = np.std(self.bwd_pkt_len_list)
+        else:
+            self.bwd_pkt_len_std = 0;
+        
+        
+        return(self.flow_duration, self.total_bytes, self.flow_byts_s)
+        
+        
+        
+
