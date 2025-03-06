73c1dd8b6e1235dd67d1c9ddebe44ef5e308bede
Krakburg <kevinroncolato@outlook.com>
2025-03-06 20:29:19 +0000

    Added settings page and setting updates

--- src/html/src/components/Settings.tsx
+++ src/html/src/components/Settings.tsx
@@ -1,10 +1,142 @@
 import { Fragment } from "react/jsx-runtime";
+import '../App.css'
+import api from "./api";
+import { useEffect, useState } from "react";
 
 function Settings() {   
+
+    interface SettingsItem {
+        capture_interface: string;
+        flow_timeout: number;
+        subflow_timeout: number;
+        model: string;
+        clear_db: boolean;
+        filtered_addresses: string[];
+        attack_threshold: number;
+    }
+
+    const [interfaceAddress, setInterfaceAddress] = useState<string>('');
+    const [flowTimeout, setFlowTimeout] = useState<number>(0);
+    const [subflowTimeout, setSubflowTimeout] = useState<number>(0);
+    const [attackThreshold, setAttackThreshold] = useState<number>(0);
+    const [model, setModel] = useState<string>('');
+    const [clearDb, setClearDb] = useState<boolean>(false);
+    const [filteredAddresses, setFilteredAddresses] = useState<string[]>([]);
+
+
+    // Fetch data from the API
+    const fetchSettings = async () => {
+        try {
+            const response = await api.get<SettingsItem>("/api/settings");
+            setInterfaceAddress(response.data.capture_interface);
+            setFlowTimeout(response.data.flow_timeout);
+            setSubflowTimeout(response.data.subflow_timeout);
+            setAttackThreshold(response.data.attack_threshold);
+            setModel(response.data.model);
+            setClearDb(response.data.clear_db);
+            setFilteredAddresses(response.data.filtered_addresses);
+
+        } catch (error) {
+            console.error("Error fetching settings:", error);
+        }
+    };
+
+    const postSettings = async () => {
+        try {
+
+            const updatedSettings: SettingsItem = {
+                capture_interface: interfaceAddress,
+                flow_timeout: flowTimeout,
+                subflow_timeout: subflowTimeout,
+                attack_threshold: attackThreshold,
+                model: model,
+                clear_db: clearDb,
+                filtered_addresses: filteredAddresses
+            };
+
+            console.log(updatedSettings)
+
+            await api.post("/api/settings", updatedSettings)
+
+        } catch (error) {
+            console.error("Error posting settings:", error);
+        }
+    }
+
+
+
+    useEffect(() => {
+        fetchSettings();
+    }, []);
+
+
     return (
         <Fragment>
             <div className="container">
-                <h1>Settings</h1>
+                <h1 className="text-center my-4">Settings</h1>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend mt-3">
+                        <span className="input-group-text" id="settings-option-text" >Interface Address</span>
+                    </div>
+                    <input type="text" className="form-control mt-3" id="interface_address" placeholder={interfaceAddress} aria-describedby="basic-addon3" onChange={(e) => setInterfaceAddress(e.target.value)}/>
+                </div>
+
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="settings-option-text">Flow Timeout</span>
+                    </div>
+                    <input type="number" className="form-control" id="flow_timeout" placeholder={String(flowTimeout)} aria-describedby="basic-addon3" onChange={(e) => setFlowTimeout(Number(e.target.value))}/>
+                </div>
+
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="settings-option-text">Subflow Timeout</span>
+                    </div>
+                    <input type="number" className="form-control" id="subflow_timeout" placeholder={String(subflowTimeout)} aria-describedby="basic-addon3" onChange={(e) => setSubflowTimeout(Number(e.target.value))}/>
+                </div>
+
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="settings-option-text">Attack Flag Threshold</span>
+                    </div>
+                    <input type="number" className="form-control" id="interface_address" placeholder={String(attackThreshold)} aria-describedby="basic-addon3" onChange={(e) => setAttackThreshold(Number(e.target.value))}/>
+                </div>
+
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                            <span className="input-group-text" id="settings-option-text">Machine Learning Model</span>
+                        </div>
+                    <select className="form-control" aria-label="Default select example" value={model} onChange={(e) => setModel(e.target.value)}>
+                        <option value="random_forest">Random Forest</option>
+                        <option value="xgboost">XGBoost</option>
+                        <option value="logistic_regression">Logistic Regression</option>
+                        <option value="k_means">K-means</option>
+                        <option value="auto_encoder">Autoencoder</option>
+                    </select>
+                </div>
+
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                            <span className="input-group-text" id="settings-option-text">Clear Database</span>
+                        </div>
+                    <select className="form-control" aria-label="Default select example" value={String(clearDb)} onChange={(e) => setClearDb(e.target.value === "true")}>
+                        <option value="false">False</option>
+                        <option value="true">True</option>
+                    </select>
+                </div>
+
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="settings-option-text">Filtered Addresses</span>
+                    </div>
+                    <input type="text" className="form-control" id="interface_address" placeholder={String(filteredAddresses)} aria-describedby="basic-addon3" onChange={(e) => setFilteredAddresses(e.target.value.split(","))}/>
+                </div>
+
+                <div className="input-group-button mb-3">
+                    <div className="input-group-prepend">
+                        <button type="button" className="btn btn-primary btn-lg" onClick={postSettings} >Save Settings</button>
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
--- src/python/main.py
+++ src/python/main.py
@@ -2,11 +2,18 @@ import packet_capture
 from manager import FlowManager
 from traffic_analyzer import TrafficAnalyzer
 import logging
-import threading
+import threading 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
+
+
+logging.basicConfig(
+    filename="logs/main.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 class Flow(BaseModel):
     src_ip: str
@@ -17,7 +24,19 @@ class Flow(BaseModel):
     attack_type: str
     probability: float
 
+class Settings(BaseModel):
+    capture_interface: str
+    flow_timeout: int
+    subflow_timeout: int
+    model: str
+    clear_db: bool
+    filtered_addresses: list[str]
+    attack_threshold: int
+
 app = FastAPI()
+
+stop_event = threading.Event()
+thread = None
 
 origins = [
     "http://localhost:5173"
@@ -33,6 +52,15 @@ app.add_middleware(
 
 flow_db = []
 device_db = dict()
+settings = {
+    "capture_interface": "192.168.0.121",
+    "flow_timeout": 600,
+    "subflow_timeout": 10,
+    "model": "random_forest",
+    "clear_db": False,
+    "filtered_addresses": ["192.168.0.91", "192.168.0.108"],
+    "attack_threshold": 15
+}
 
 @app.get("/api/flows")
 def get_flows():
@@ -42,64 +70,88 @@ def get_flows():
 def get_devices():
     return (device_db)
 
-logging.basicConfig(
-    filename="logs/main.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
+@app.get("/api/settings")
+def get_settings():
+    return (settings)
+
+@app.post("/api/settings")
+def set_settings(setting: Settings):
+    global thread, flow_db, device_db
+
+    # Update Settings
+    settings.update(setting)
+
+    # Check for clear DB
+    if settings["clear_db"] == True:
+        flow_db = []
+        device_db = dict()
+
+    # Stop Thread
+    stop_event.set()
+    thread.join()
+
+    # Restart Thread
+    thread = threading.Thread(target=main_app)
+    stop_event.clear()
+    thread.start()
+
+
 
 def main_app():
+    # Setup
     print("Starting Capture...")
-    capture = packet_capture.start("192.168.0.108")
-    flow_manager = FlowManager(600, 10)
+    capture = packet_capture.start(settings["capture_interface"])
+    flow_manager = FlowManager(settings["flow_timeout"], settings["subflow_timeout"])
     traffic_analyzer = TrafficAnalyzer("model/rf_ddos.pkl", "model/le_ddos.pkl", "model/lime_explainer_ddos.pkl")
     print("Capturing Packets...")
 
+    # Create output CSV
     f = open("logs/output_data.csv", "w+")
     f.write("dst_port,proto,flow_duration_ms,tot_fwd_pkts,tot_bwd_pkts,total_fwd_pkt_len,total_bwd_pkt_len,fwd_pkt_len_max,fwd_pkt_len_min,fwd_pkt_len_mean,fwd_pkt_len_std,bwd_pkt_len_max,bwd_pkt_len_min,bwd_pkt_len_mean,bwd_pkt_len_std,flow_byts_s,flow_pkts_s,flow_iat_mean,flow_iat_std,flow_iat_max,flow_iat_min,fwd_iat_total,fwd_iat_mean,fwd_iat_std,fwd_iat_max,fwd_iat_min,bwd_iat_total,bwd_iat_mean,bwd_iat_std,bwd_iat_max,bwd_iat_min,fwd_psh_flag,bwd_psh_flag,fwd_urg_flag,bwd_urg_flag,fwd_header_len,bwd_header_len,flow_fwd_pkts_s,flow_bwd_pkts_s,pkt_len_min,pkt_len_max,pkt_len_mean,pkt_len_std,pkt_len_var,fin_flag_cnt,syn_flag_cnt,rst_flag_cnt,psh_flag_cnt,ack_flag_cnt,urg_flag_cnt,pkt_size_avg,fwd_seg_size_avg,bwd_seg_size_avg,subflow_fwd_pkts,subflow_fwd_bytes,subflow_bwd_pkts,subflow_bwd_bytes,init_fwd_win_bytes,init_bwd_win_bytes,fwd_act_data_pkts,fwd_seg_size_min,active_mean,active_std,active_max,active_min,idle_mean,idle_std,idle_max,idle_min")
     f.write('\n')
     f.close()
 
+    # Capture
     try:
-        while True:
+        while not stop_event.is_set():
             if packet_capture.has_next():
                 packet = packet_capture.get_packet()
 
-                #if packet.src_ip == "192.168.0.107" or packet.dst_ip == "192.168.0.107":
+                if packet.src_ip not in settings["filtered_addresses"]:
 
-                flow_manager.handle_packet(packet)
-                flow_manager.check_flow_timeout()
-                flow_manager.list_flows()
+                    flow_manager.handle_packet(packet)
+                    flow_manager.check_flow_timeout()
+                    flow_manager.list_flows()
 
-                flow_data = flow_manager.get_flow_data()
-                
-                if flow_data:
-                    result = traffic_analyzer.get_prediction(flow_data)
-
-                    print(result)
+                    flow_data = flow_manager.get_flow_data()
                     
-                    result_val = result[:, 1].item()*100
+                    if flow_data:
+                        result = traffic_analyzer.get_prediction(flow_data)
 
-                    ip = flow_data["src_ip"]
-
-                    flow_db.append(Flow(src_ip=ip, dst_ip=flow_data["dst_ip"], src_port=flow_data["src_port"], dst_port=flow_data["dst_port"], protocol=flow_data["proto"], attack_type="placeholder", probability=result_val))
+                        print(result)
                         
-                    good_flow , bad_flow = (0, 1) if result_val >= 15 else (1, 0)
+                        result_val = result[:, 1].item()*100
 
-                    if ip not in device_db:
-                        device_db[ip] = {
-                            "tot_fwd_pkts": flow_data["tot_fwd_pkts"],
-                            "tot_bwd_pkts": flow_data["tot_bwd_pkts"],
-                            "good_flows": good_flow,
-                            "bad_flows": bad_flow,
-                            "total_flows": 1
-                        }
-                    else:
-                        device_db[ip]["tot_fwd_pkts"] += flow_data["tot_fwd_pkts"]
-                        device_db[ip]["tot_bwd_pkts"] += flow_data["tot_bwd_pkts"]
-                        device_db[ip]["good_flows"] += good_flow
-                        device_db[ip]["bad_flows"] += bad_flow
-                        device_db[ip]["total_flows"] += 1
+                        ip = flow_data["src_ip"]
+
+                        flow_db.append(Flow(src_ip=ip, dst_ip=flow_data["dst_ip"], src_port=flow_data["src_port"], dst_port=flow_data["dst_port"], protocol=flow_data["proto"], attack_type="placeholder", probability=result_val))
+                            
+                        good_flow , bad_flow = (0, 1) if result_val >= settings["attack_threshold"] else (1, 0)
+
+                        if ip not in device_db:
+                            device_db[ip] = {
+                                "tot_fwd_pkts": flow_data["tot_fwd_pkts"],
+                                "tot_bwd_pkts": flow_data["tot_bwd_pkts"],
+                                "good_flows": good_flow,
+                                "bad_flows": bad_flow,
+                                "total_flows": 1
+                            }
+                        else:
+                            device_db[ip]["tot_fwd_pkts"] += flow_data["tot_fwd_pkts"]
+                            device_db[ip]["tot_bwd_pkts"] += flow_data["tot_bwd_pkts"]
+                            device_db[ip]["good_flows"] += good_flow
+                            device_db[ip]["bad_flows"] += bad_flow
+                            device_db[ip]["total_flows"] += 1
 
     except KeyboardInterrupt:
         print("\nExiting...")
@@ -107,9 +159,11 @@ def main_app():
     except Exception as e:
         print(f"Error: {e}")
 
+
 if __name__ == "__main__":
 
     thread = threading.Thread(target=main_app)
+
     thread.start()
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
